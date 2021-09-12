ce39a5ebca5a81210f19832162ffa86e7070c735
David Les <leboy1952@gmail.com>
2021-09-12 17:23:14 +0700

    I have done (basic_operator, conditions, lists, loop, string,variable.)

---
+++ lists.py
@@ -0,0 +1,12 @@
+numbers = [1,2,3]
+strings = ['helo','world']
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
